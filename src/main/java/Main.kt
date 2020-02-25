4445403b847b603fb1a74151b89748744081fa1d
Christoph <zort0s420@gmail.com>
2020-02-25 13:25:50 +0100

    Added time counter

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -3,27 +3,30 @@ import Dataset.Dataset
 import DatasetGenerator.XOR
 import NeuralNetwork.NeuralNetwork
 import kotlin.concurrent.thread
-
+import kotlin.coroutines.*;
+import kotlin.system.measureTimeMillis
 
 fun main(){
-/*
+
+
   val af: ActivationFunction = TanH()
   val nn = NeuralNetwork(0.3, af, 2, 4, 2, 1)
   val dataset = Dataset()
   val xor = XOR()
+  val timeNeeded = measureTimeMillis {
   dataset.generate(xor, 1_000)
   repeat(20_000) {
     nn.trainBatch(128, dataset)
+  }
   }
   println(nn.feedForward(arrayOf(-1.0,-1.0))[0])
   println(nn.feedForward(arrayOf(-1.0,1.0))[0])
   println(nn.feedForward(arrayOf(1.0,-1.0))[0])
   println(nn.feedForward(arrayOf(1.0,1.0))[0])
-*/
-  for(i in 0..100_000_000)
-
+    println(timeNeeded)
 
 }
+
 
 
 
